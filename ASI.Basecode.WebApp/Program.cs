81f1f3b9d601621377fe7a6ce9156ded62aea497
laloweeee <osabellowe@gmail.com>
2025-10-14 16:48:26 +0800

    program.cs small change

--- ASI.Basecode.WebApp/Program.cs
+++ ASI.Basecode.WebApp/Program.cs
@@ -29,9 +29,11 @@ appBuilder.Logging
 var configurer = new StartupConfigurer(appBuilder.Configuration);
 configurer.ConfigureServices(appBuilder.Services);
 
+// Register DbContext
 appBuilder.Services.AddDbContext<AsiBasecodeDBContext>(options =>
     options.UseSqlServer(appBuilder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Register Staff Repository and Service
 appBuilder.Services.AddScoped<IStaffRepository, StaffRepository>();
 appBuilder.Services.AddScoped<IStaffService, StaffService>();
 
